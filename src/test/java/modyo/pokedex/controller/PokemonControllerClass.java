6d8f6a91668d11c0825734d33a2432aec4c2a51e
Pablo Aravena <pablo.aravena.n@gmail.com>
2021-07-30 17:42:26 -0400

    Added Unit Tests

--- src/test/java/modyo/pokedex/controller/PokemonControllerClass.java
+++ src/test/java/modyo/pokedex/controller/PokemonControllerClass.java
@@ -4,13 +4,11 @@ package modyo.pokedex.controller;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
